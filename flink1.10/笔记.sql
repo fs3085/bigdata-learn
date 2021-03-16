4462af3dcba3048b597c31aad0145b478642021a
yangweifan <yangweifan@wecreditlife.com>
2021-03-16 10:03:36 +0800

    update flink笔记 clickhouse模块 hbase笔记

---
+++ flink1.10/笔记.sql
@@ -0,0 +1,52 @@
+1.hiveserver2链接
+beeline -u jdbc:hive2://dxbigdata102:10000 -n hive
+
+2.分布式思想，sqoop mysql jar 每台机器都要上传
+
+3.mysql8.0版本以下不支持使用lag,lead函数
+LAG
+LAG(col,n,DEFAULT) over(partition by 字段 order by 字段)用于统计窗口内往上第n行值
+第一个参数为列名，
+第二个参数为往上第n行（可选，默认为1），
+第三个参数为默认值（当往上第n行为NULL时候，取默认值，如不指定，则为NULL）
+
+LEAD
+与LAG相反
+LEAD(col,n,DEFAULT) over(partition by 字段 order by 字段) 用于统计窗口内往下第n行值
+第一个参数为列名，
+第二个参数为往下第n行（可选，默认为1），
+第三个参数为默认值（当往下第n行为NULL时候，取默认值，如不指定，则为NULL）
+
+
+用mysql变量实现lag函数功能
+SELECT base.id
+      ,base.before_name
+      ,base.name
+      ,base.time
+  FROM (SELECT IF(@id = t.id, @lagname := @NAME, @lagname := '') AS before_name
+              ,@id := t.id AS tid
+              ,@NAME := t.name AS tafter_name
+              ,t.*
+          FROM (SELECT *
+                  FROM t_order
+                 ORDER BY id
+                         ,TIME) t
+              ,(SELECT @lagname := NULL
+                      ,@id      := 0
+                      ,@NAME    := NULL) r) base;
+mysql变量：
+为变量赋初始值
+(SELECT @lagname := NULL
+                      ,@id      := 0
+                      ,@NAME    := NULL) r
+源表数据
+(SELECT *
+                  FROM t_order
+                 ORDER BY id
+                         ,TIME) t
+
+每一行遍历一次，变量的值可以记录
+SELECT IF(@id = t.id, @lagname := @NAME, @lagname := '') AS before_name
+              ,@id := t.id AS tid
+              ,@NAME := t.name AS tafter_name
+              ,t.*
