96af265b92231c29be354052603bed6a27aecff6
yangweifan <yangweifan@wecreditlife.com>
2021-08-10 17:49:45 +0800

    es_springdata

---
+++ elasticsearch/src/main/java/es_springdata/ProductDao.java
@@ -0,0 +1,8 @@
+package es_springdata;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductDao extends ElasticsearchRepository<Product,Long> {
+}
---
+++ elasticsearch/src/main/java/es_springdata/SpringDataElasticSearchMainApplication.java
@@ -0,0 +1,14 @@
+package es_springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataElasticSearchMainApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringDataElasticSearchMainApplication.class,args);
+
+    }
+}
